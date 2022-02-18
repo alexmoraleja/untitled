e32bf9c518aa3d684771289d7c25b3f403f49305
alexmoraleja <99951166+alexmoraleja@users.noreply.github.com>
2022-02-18 08:57:13 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,13 @@
+basic.showNumber(3)
+basic.showNumber(2)
+basic.showNumber(1)
+basic.forever(function () {
+    basic.showIcon(IconNames.Scissors)
+    basic.showLeds(`
+        # # . . .
+        # # . . .
+        . . # # #
+        # # . . .
+        # # . . .
+        `)
+})
